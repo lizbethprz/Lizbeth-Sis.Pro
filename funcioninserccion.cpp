ddeaaaaeffc4dbf0cfb27ab104bdc366e10e3d00
lizbethprz <60414195+lizbethprz@users.noreply.github.com>
2020-02-04 17:04:19 -0800

    Add files via upload

---
+++ funcioninserccion.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  double xu,xd,yu,yd,re1,re2,inser,pendiente;
+ Cout<<"Inserte su primer par de números xu";
+  cin>> xu;
+  Cout<<"Inserte su primer par de números yu";
+  std::cin >> ;>> yu;
+
+  Cout<<"Inserte su segundo par de números xd";
+  cin>> xd;
+  Cout<<"Inserte su segundo par de números yd";
+  cin>> yd;
+
+  re1=(xu+xd)/2;
+  re2=(yu+yd)/2;
+
+  pendiente=(yd-yu)/(xd-xu)
+ Cout<<"Inserte un punto de insercion: ";
+ std::cin >> ;>> inser;
+ Cout<<"el resultado es igual a Y="+pendiente"x+"inser<<endl
+
+    return 0;
+}
---
+++ funcionpendiente.cpp
@@ -0,0 +1,36 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double xu,xd,yu,yd,re1,re2,re3,re4,pendiente;
+ std::cout<<"Inserte su primer par de números xu";
+  std::cin>> xu;
+  std::cout<<"Inserte su primer par de números yu";
+  std::cin>> yu;
+
+  std::cout <<  << std::endl;t<<"Inserte su segundo par de números xd";
+  std::cin >> ;n>> xd;
+  coutt<<"Inserte su segundo par de números yd";
+  std::cin >> ;n>> yd;
+
+  re1=(xu+xd)/2;
+  re2=(yu+yd)/2;
+
+  pendiente=(yd-yu)/(xd-xu)
+
+  re3=pendiente*xu
+  re4 = re3/yu
+std::cout<<"el resultado es igual a Y="+pendiente"x+"+re4<<endl
+
+    return 0;
+}
